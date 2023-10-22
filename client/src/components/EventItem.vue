a94c2cef30574d66b7f256794dcc2b6348b34127
mnenie <121057011+mneniee@users.noreply.github.com>
2023-10-22 04:10:43 +0300

    update

--- client/src/components/EventItem.vue
+++ client/src/components/EventItem.vue
@@ -80,11 +80,7 @@ onMounted(() => {
     </div>
     <div class="second">
       <span class="size_5 desc">{{ info?.event.description }}</span>
-<<<<<<< HEAD
-      <span style="margin-bottom: 20px;" class="size_5">Автор:  {{ info?.organaizer }} </span>
-=======
       <span style="margin-bottom: 20px;" class="size_5">Автор: {{ info?.organizer.firstName }} </span>
->>>>>>> c5c2e86047176b2c8633278113ea6ac2c2603427
       <btn-event @click="showAlert" style="align-self: flex-start;">Я пойду</btn-event>
     </div>
     <div class="round"></div>
